6b7a85ef762eaddda9c1c9c7fb77c0191735824f
Isaí Pérez Ayón <isaiayon23@gmail.com>
2024-06-18 16:36:32 -0600

    Terminado formulario y comprobaciones

---
+++ js/app.js
@@ -0,0 +1,28 @@
+/* 
+* Js para la comprobación de datos del formulario 
+*/
+
+//Ininicializar objerotos del DOM
+const nickInput = document.getElementById('nick');
+const tamanotInput = document.getElementById('tamano');
+const formEntrada = document.getElementById('formEntrada');
+const error = document.getElementById('error');
+
+//Funciones de eventos
+function comprobarForm(event){
+    if(nickInput.value.match(/(?<!\S)[0-9]/)){
+        nickInput.focus();
+        event.preventDefault();
+        error.innerText="El campo de nick no puede comenzar con un numero";
+        return false;
+    }else if(tamanotInput.value == "0"){
+        tamanotInput.focus();
+        event.preventDefault();
+        error.innerText = 'Se debe seleccionar el tamaño del panel'
+        return false;
+    }return true
+
+}
+
+//Inicio de carga de eventos
+formEntrada.addEventListener('submit', comprobarForm);
